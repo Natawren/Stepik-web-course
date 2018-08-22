91a853a131db65c7e5c967f4c432bc088e007bba
Natawren <42349936+Natawren@users.noreply.github.com>
2018-08-22 22:21:50 +0300

    changes 22.08

--- hello.py
+++ hello.py
@@ -2,4 +2,8 @@ def application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
-    return "\n".join(environ.get('QUERY_STRING').split("&"))
+    body = '\r\n'.join(environ['QUERY_STRING'].split('&'))
+    return body
+
+
+   
