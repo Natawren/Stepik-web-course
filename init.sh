84aafadea3cafb77502bce645fa23f4c6650351c
Natawren <42349936+Natawren@users.noreply.github.com>
2018-08-22 22:20:26 +0300

    changes 22.08

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm -r /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
