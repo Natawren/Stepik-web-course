552f6d36744b1c998240c15cbe579ffbaa802341
Natawren <42349936+Natawren@users.noreply.github.com>
2018-08-30 11:59:06 +0300

    30.08

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 
 
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('qa.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/$', 'qa.views.test'),
+    url(r'^signup/$', 'qa.views.test'),
+    url(r'^question/\d+$', 'qa.views.test'),
+    url(r'^ask/$', 'qa.views.test'),
+    url(r'^popular/$', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test'),
+    url(r'^admin/', include(admin.site.urls)),
 ]
