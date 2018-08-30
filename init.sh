566da942c00e3bed3de4c248c91ca96be4ccfe1d
Natawren <42349936+Natawren@users.noreply.github.com>
2018-08-30 14:39:11 +0300

    update

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test/
+sudo -s ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/gunicorn.conf
+sudo -s ln -sf /home/box/web/etc/django_gunicorn.conf  /etc/gunicorn.d/django_gunicorn.conf
 sudo /etc/init.d/gunicorn restart
