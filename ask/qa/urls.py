c5354fa374d2c75d3f683d6e0b941c3d7f23eac5
Natawren <42349936+Natawren@users.noreply.github.com>
2018-08-30 12:00:45 +0300

    30.08

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,6 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.test, name='index'),
-    url(r'^login/$', views.test, name = 'login').
-    url(r'^signup/$', views.test, name = 'signup'),
-    url(r'^ask/$', views.test, name = 'ask'),
-    url(r'^popular/$', views.test, name = 'popular'),
-    url(r'^new/$', views.test, name = 'new'),
-    url(r'^question/(?P<id>\d+)/$', views.test, name = 'question'),
-    
+    url(r'^$',views.test, name = 'index'),
 ]
