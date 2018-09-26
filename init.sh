05650e899c8b0a7213e22bddcecc6a005e455263
Natawren <42349936+Natawren@users.noreply.github.com>
2018-09-26 12:46:14 +0300

    26.09

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+cd ~/web/ask/ask
 sudo gunicorn ask.wsgi:application --bind 0.0.0.0:8000
 
 sudo /etc/init.d/mysql start
