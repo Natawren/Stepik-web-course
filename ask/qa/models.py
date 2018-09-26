66e821403484a627276fe652d123bf038843782a
Natawren <42349936+Natawren@users.noreply.github.com>
2018-09-26 12:39:35 +0300

    with QM

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,7 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):                                          
+        def new():                                                              
+                pass                                                            
+        def popular():                                                          
+                pass 
+          
 class Question(models.Model):
+  objects = QuestionManager() 
   title = models.CharField(max_length = 255)
   text = models.TextField()
   added_at = models.DateField(blank = True)
