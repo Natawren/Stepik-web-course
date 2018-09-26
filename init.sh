8af9344a5cc15487668f6a5ed5637c3a94481c74
Natawren <42349936+Natawren@users.noreply.github.com>
2018-09-26 13:12:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd ~/web/ask/ask
-sudo gunicorn ask.wsgi:application --bind 0.0.0.0:8000
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:application     
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application 
