a29376859524cdbdbbc3159e0458a3115aeca66f
Natawren <42349936+Natawren@users.noreply.github.com>
2018-09-26 12:57:14 +0300

    deleted info about sql

--- init.sh
+++ init.sh
@@ -4,8 +4,3 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd ~/web/ask/ask
 sudo gunicorn ask.wsgi:application --bind 0.0.0.0:8000
-
-sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE djangobase;"
-mysql -uroot -e "CREATE USER 'gdb@localhost' IDENTIFIED BY 'password';"
-mysql -uroot -e "GRANT ALL ON djangobase.* TO 'gdb@localhost';"
