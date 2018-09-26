2a8425f75b0b1c3f0c355f86c2b116e126acd2ae
Natawren <42349936+Natawren@users.noreply.github.com>
2018-09-26 12:08:29 +0300

    26/09 sql

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
+
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/django_gunicorn.conf /etc/gunicorn.d/test-django
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn ask.wsgi:application --bind 0.0.0.0:8000
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE djangobase;"
+mysql -uroot -e "CREATE USER 'gdb@localhost' IDENTIFIED BY 'password';"
+mysql -uroot -e "GRANT ALL ON djangobase.* TO 'gdb@localhost';"
