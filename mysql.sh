d5ad2583976dd3180e33717ba42af84c3e374cb7
Natawren <42349936+Natawren@users.noreply.github.com>
2018-09-26 13:09:31 +0300

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE djangobase;"
-mysql -uroot -e "CREATE USER 'gdb@localhost' IDENTIFIED BY 'password';"
-mysql -uroot -e "GRANT ALL ON djangobase.* TO 'gdb@localhost';"
+mysql -uroot -e "CREATE USER 'gdb'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -e "GRANT ALL ON djangobase.* TO 'gdb'@'localhost';"
