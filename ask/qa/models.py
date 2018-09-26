22b5904a2dd81cd8712404fc9d2aa7bf2ccae297
Natawren <42349936+Natawren@users.noreply.github.com>
2018-09-26 12:37:45 +0300

    26.09. adding models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+  title = models.CharField(max_length = 255)
+  text = models.TextField()
+  added_at = models.DateField(blank = True)
+  rating = models.IntergerField()
+  author = models.ForeignKey(User, null = True, on_delete = models.SET_NULL)
+  likes = models.ManyToManyField(Likes)
+  
+  def __str__(self):
+    return self.title
+  def get_absolute_url(self):
+    return '/question/%d/' %self.pk
+
+class Answer(models.Model):
+  text = models.TextField()
+  added_at = models.DateField(blank = True)
+  question = models.ForeignKey(Question, null = True, on_delete = models.SET_NULL)
+  author = models.ForeignKey(User, null = True, on_delete = models.SET_NULL)
+  
+  def __str__(self):
+    return self.text
